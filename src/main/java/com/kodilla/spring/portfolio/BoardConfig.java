b4cad2439d02c1508de9080c009426ae61666f56
unknown <amienkowski@gmail.com>
2020-10-05 21:19:12 +0200

    14.3 - wersja gotowa

--- src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.portfolio;
 
-import com.kodilla.spring.reader.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -9,47 +8,36 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
-//    @Autowired
-//    @Qualifier("toDoList")
-//    TaskList toDoList;
-//    @Autowired
-//    @Qualifier("inProgressList")
-//    TaskList inProgressList;
-//    @Autowired
-//    @Qualifier("doneList")
-//    TaskList doneList;
     @Autowired
-    @Qualifier("newTaskList")
-    TaskList newTaskList;
+    @Qualifier("toDoList")
+    TaskList toDoList;
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
 
     @Bean
-//    public Board createBoard() {
-//        return new Board(toDoList, inProgressList, doneList);
-//    }
     public Board createBoard() {
-        return new Board(newTaskList, newTaskList, newTaskList);
+        return new Board(toDoList, inProgressList, doneList);
     }
-//    @Bean(name = "toDoList")
-//    @Scope("prototype")
-//    public TaskList getToDoList() {
-//        return new TaskList();
-//    }
-//
-//    @Bean(name = "inProgressList")
-//    @Scope("prototype")
-//    public TaskList getInProgressList() {
-//        return new TaskList();
-//    }
-//
-//    @Bean(name = "doneList")
-//    @Scope("prototype")
-//    public TaskList getDoneList() {
-//        return new TaskList();
-//    }
 
-    @Bean(name = "newTaskList")
+    @Bean(name = "toDoList")
     @Scope("prototype")
-    public TaskList getNewTaskList() {
+    public TaskList getToDoList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    @Scope("prototype")
+    public TaskList getInProgressList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "doneList")
+    @Scope("prototype")
+    public TaskList getDoneList() {
         return new TaskList();
     }
 }
